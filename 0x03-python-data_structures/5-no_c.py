1b9bccb720f7d6f804f82c2d42907f09891a8f19
Andrés Medina <andresmedinaph@gmail.com>
2022-01-04 04:11:07 -0500

    Adding no c excercise

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    size = len(my_string)
+    for i in range(0, size):
+        if my_string[i] != 'c' and my_string[i] != 'c':
+            new_string += my_string[i]
+    return new_string
