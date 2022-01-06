ddc40da0542506206d606a146c20c2e6cea2d63b
Andrés Medina <andresmedinaph@gmail.com>
2022-01-05 20:22:10 -0500

    Clean commit vol 2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    score = 0
+    score_key = ""
+    for key, value in a_dictionary.items():
+        if value > score:
+            score_key = key or None
+            score = value
+    return score_key
