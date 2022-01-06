c336317cb2ae21dad1e409578a5a64a37a297d10
Andrés Medina <andresmedinaph@gmail.com>
2022-01-05 19:42:32 -0500

    Clean commit vol 1

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = sum(number for number in set(my_list))
+    return result
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_dictionary = a_dictionary
+    for keys, values in sorted(sorted_dictionary.items()):
+        print("{:s}: {}".format(keys, values))
