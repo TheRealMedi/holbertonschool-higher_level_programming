6b56432942b09a733e226e940aae5437d7a4e25e
Andrés Medina <andresmedinaph@gmail.com>
2022-01-17 21:20:42 -0500

    Adding three task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-from typing import Counter
-
-
 def safe_print_list(my_list=[], x=0):
-    Counter = 0
+    counter = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
-            Counter += 1
+            counter += 1
         except IndexError:
             break
     print()
-    return Counter
+    return counter
