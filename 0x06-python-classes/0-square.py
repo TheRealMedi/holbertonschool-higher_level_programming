82003d180ef45ca83585bad60f6aeac653465037
Andres Medina <medios@Andress-MacBook-Pro.local>
2022-01-18 22:21:44 -0500

    Adding RAWFILES & First 2 tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Empty class that defines a square"""
+class Square:
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Class wich defines a square with a private instance attribute size"""
+class Square:
+    def __init__(self, size):
+        self.__size = size
