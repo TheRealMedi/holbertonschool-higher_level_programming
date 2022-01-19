835a091f98b2b6c441893ae04269ed4677ccca9d
Andres Medina <medios@Andress-MacBook-Pro.local>
2022-01-18 22:27:47 -0500

    Adding some docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Class wich defines a square with a private instance attribute size."""
+
 class Square:
     """Square class representation."""
+
     def __init__(self, size):
         """Intializating class
-        with size argument."""
+        with size argument.
+        """
         self.__size = size
