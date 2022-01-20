5b80b806153522e22ff8a4a61d4cd41e7439bf28
Andres Medina <medios@Andress-MacBook-Pro.local>
2022-01-19 22:32:26 -0500

    Adding 2nd & 3rd tasks

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Empty class wich defines a square."""
+
+
+class Square:
+    """Representation of Square class."""
+
+    def __init__(self, size=0):
+        """Initializing Square.
+        """
+        if type(size) is not int:
+                raise TypeError("size must be an integer")
+        if size < 0:
+                raise ValueError("size must be >= 0")
+        self.size=size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Empty class wich defines a square."""
+
+
+class Square:
+    """Representation of square class"""
+
+    def __init__(self, size=0):
+        """Intializing new square
+        """
+        if type(size, int) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size * self.__size
