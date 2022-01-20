83c048956dfbc6a15d6fcd3c1d9001c0b0c13b47
Andres Medina <medios@Andress-MacBook-Pro.local>
2022-01-19 23:02:19 -0500

    Adding 4th & 5th tasks

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Empty class wich defines a square."""
+
+
+class Square:
+    """Representation of Square class."""
+
+    def __init__(self, size=0):
+        """Initializing Square
+        """
+        self.__size = size
+
+    def area(self):
+        """Public instance wich returns the area."""
+        return(self.__size * self.__size)
+
+    @property
+    def size(self):
+        """returns the size."""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
