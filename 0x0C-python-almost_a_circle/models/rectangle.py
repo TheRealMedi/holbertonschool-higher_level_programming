9b3799ff6e3ba9f59d8c939041c3fdfe03e70bfc
Andres Medina <medios@Andress-MacBook-Pro.local>
2022-02-08 01:51:06 -0500

    Adding str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,8 @@ class Rectangle(Base):
         for i in range(self.height):
             print(" " * self.x, end='')
             print("#" * self.width)
+
+    def __str__(self):
+        """"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
