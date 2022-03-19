7982484f64fc21565db18ac545d4304caeccc2be
Andrés Medina <andresmedinaph@gmail.com>
2022-03-18 20:47:28 -0500

    Testing

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Deltes the 'hbtn_0c_0' database.
-DROP DATABASE IF EXISTS 'hbtn_0c_0';
+DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
 -- Creates a table called 'first_table' with id and name values.
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT, 
+    name VARCHAR(256)
+);
