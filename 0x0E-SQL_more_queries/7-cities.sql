d2e1416ebe37f3d08908191fbad6db4a73c75dcb
Andrés Medina <andresmedinaph@gmail.com>
2022-03-20 06:15:58 -0500

    HF

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 --
 -- Creating database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Creating table
+-- create the table now
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES (hbtn_0d_usa.states(id))
-) ENGINE = INNOBD;
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+) ENGINE=INNODB;
