3e452567aa5f3a11d27ccd93524307c4d5df9ca1
Andrés Medina <andresmedinaph@gmail.com>
2022-03-20 05:40:29 -0500

    fixing

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creating User2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Granting Privileges
-GRANT SELECT ON hbtn_0d_2* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
