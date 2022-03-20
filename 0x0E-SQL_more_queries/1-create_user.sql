391f64bc69c38ef4f3e724e0b1a95fb7a0408ee9
Andrés Medina <andresmedinaph@gmail.com>
2022-03-20 04:54:21 -0500

    HF

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Creates an user
 -- Creating the user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localshot' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Granting Privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localshot';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 -- Flushing
 FLUSH PRIVILEGES;
