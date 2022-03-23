7c717372dbc52e31079f2eb911361b23dd53b131
Medi <medi@192.168.0.13>
2022-03-23 17:20:36 -0500

    HF

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,11 @@
 -- Creates 'second-table' and the fill it with id, names & scores.
-CREATE TABLE IF NOT EXISTS second_table (
+USECREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 -- inserting rows
-INSERT INTO second_table
-VALUES (1, "Jhon", 10),
-        (2, "Alex", 3),
-        (3, "Bob", 14),
-        (4, "George", 8);
+INSERT INTO 'second_table' ('id','name','score') VALUES (1, "John", 10);
+INSERT INTO 'secont_table' ('id','name','score') VALUES (2, "Alex", 3);
+INSERT INTO 'second_table' ('id','name','score') VALUES (3, "Bob", 14);
+INSERT INTO 'second_table' ('id','name','score') VALUES (4, "George", 8);
