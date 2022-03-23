e31f8de3a7341686cb1f922c57f23b9292f6a895
Medi <medi@192.168.0.13>
2022-03-23 17:38:16 -0500

    Hotfix3

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- Computes the aerage from the score records
-SELECT AVG('score') AS "average" FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Number per score
 -- select count of score
-SELECT score, COUNT(score) AS "number" FROM 'second_table' GROUP BY 'score' ORDER BY 'score' DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
