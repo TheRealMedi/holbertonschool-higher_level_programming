cbd53628fe2233b678b8c649772b1b41e2f841cd
Medi <medi@Medis-MacBook-Pro.local>
2022-04-04 19:52:14 -0500

    Trying df way

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const s = process.argv.length;
-console.log(s == 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+console.log(s === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
